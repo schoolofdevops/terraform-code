6948572c96c1ae7abe38cb1dc94269bcbf693677
Gourav Shah <gs@initcron.org>
2018-11-21 17:38:46 +0530

    clean up

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devopsdemo-tf-backend"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "devops-demo-table"
-  }
-}
